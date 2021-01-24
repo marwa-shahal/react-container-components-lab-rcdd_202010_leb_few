b2dc78c2cba5389178e1e0322cfb66748a51ba58
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-24 17:31:02 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -26,11 +26,7 @@ class LatestMovieReviewsContainer extends Component {
   
   render(){
     return (
-<<<<<<< HEAD
       <div className="latest-movie-reviews">
-=======
-       <div className="latest-movie-reviews">
->>>>>>> da20bf9b4a16d26775eac0cd4df51fc8e9acbcf0
        <h1>Latest Movies Reviews</h1>
        <MovieReviews reviews={this.state.reviews} />
       </div>
